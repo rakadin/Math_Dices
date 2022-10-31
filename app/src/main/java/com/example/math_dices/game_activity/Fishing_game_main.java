732caa2f52a0da7ba6568fb99e01ab3e01b37301
PhamXuanDuc <phamduc04102001@gmail.com>
2022-10-31 11:32:54 +0700

    thêm game câu cá 1

---
+++ app/src/main/java/com/example/math_dices/game_activity/Fishing_game_main.java
@@ -0,0 +1,17 @@
+package com.example.math_dices.game_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.math_dices.R;
+
+public class Fishing_game_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fishing_game_main);
+        getSupportActionBar().hide();
+    }
+}
