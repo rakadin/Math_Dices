a51b715faed5a037ae78605312b1c48706327f79
casaucon <Trantoan127.vp@gmail.com>
2022-11-14 00:53:54 +0700

    commit card game layout

---
+++ app/src/main/java/com/example/math_dices/game_activity/Card_game_main.java
@@ -0,0 +1,13 @@
+package com.example.math_dices.game_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Card_game_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/math_dices/introductions/Card_introduction.java
@@ -0,0 +1,13 @@
+package com.example.math_dices.introductions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Card_introduction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
