bde993316bd49265d555de2bb342837911921f9f
hoangnhathn <hoangnhathn1702@gmail.com>
2022-12-01 18:15:53 +0700

    them chuc nang cho trò chơi bắt gà

--- app/src/main/java/com/example/math_dices/introductions/Chicken_introdiction.java
+++ app/src/main/java/com/example/math_dices/introductions/Chicken_introdiction.java
@@ -2,12 +2,15 @@ package com.example.math_dices.introductions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.math_dices.R;
 import com.example.math_dices.controller.SoundControll;
+import com.example.math_dices.game_activity.Chicken_game_main;
 
 public class Chicken_introdiction extends AppCompatActivity {
     Button nextBut;
@@ -19,5 +22,37 @@ public class Chicken_introdiction extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chicken_game_introduction);
         getSupportActionBar().hide();
+        onoffBut = findViewById(R.id.SonoffBut2);
+        nextBut = findViewById(R.id.nextBut2);
+        soundControll.OnOffFun(Chicken_introdiction.this,onoffBut);
+
+        // next game 1 main activity function
+        nextBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundControll.PopSoundFun2(view.getContext(),nextBut);
+                Intent intent = new Intent();
+                intent.setClass(view.getContext() , Chicken_game_main.class);
+                startActivity(intent);
+                soundControll.player.pause();
+            }
+        });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        soundControll.player.stop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        soundControll.player.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        soundControll.player.stop();
     }
 }
